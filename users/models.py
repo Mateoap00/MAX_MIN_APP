a52ad1d8e8173eefbb64343804f29db21c8d37da
mateoap00 <mateo.a.p.2000@gmail.com>
2023-07-03 15:55:12 -0500

    Django MaxMinApp first commit, login and register users

---
+++ users/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.utils import timezone
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, password, **other_fields):
+        if not email:
+            raise ValueError('Email is required')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, email, username, password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        return self.create_user(email, username, password, **other_fields)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, unique=True)
+    start_date = models.DateTimeField(default=timezone.now)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.username
---
+++ users/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .models import CustomUser
+
+# Create your views here.
+def loginUser(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            print('Login succeed!')
+            # login(request, user)
+            return redirect('index')
+        else:
+            print('Login failed with wrong credentials!')
+            return redirect('login')
+    else:
+        return render(request, 'users/login.html', {})
+    
+def registerUser(request):
+    if request.method == 'POST':
+        email = request.POST["email"]
+        username = request.POST["username"]
+        password = request.POST["password1"]
+        user = CustomUser.objects.create_user(email=email, username=username, password=password)
+        if user is not None:
+            print('Registration succeed!')
+            return redirect('login')
+        else:
+            print('Registration failed!')
+            return redirect('register')
+    else:
+        return render(request, 'users/register.html', {})
+    
+def logoutUser(request):
+    if request.method == 'GET':
+        logout(request)
+        print('Logout succeed!')
+        return redirect('index')
